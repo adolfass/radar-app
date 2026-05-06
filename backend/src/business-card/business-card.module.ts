b8bf3906d71232bffeea4a48ce0e3e8b76054c06
adolfass <romabo51@gmail.com>
2026-05-06 13:53:29 +0300

    fix: Import ContactModule in BusinessCardModule

--- backend/src/business-card/business-card.module.ts
+++ backend/src/business-card/business-card.module.ts
@@ -3,9 +3,10 @@ import { BusinessCardController } from './business-card.controller';
 import { BusinessCardService } from './business-card.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserModule } from '../user/user.module';
+import { ContactModule } from '../contact/contact.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, ContactModule],
   controllers: [BusinessCardController],
   providers: [BusinessCardService],
   exports: [BusinessCardService],
